d81b08a73a89c550f2d6d2f5d7722711ffb9951a
Jörg Lauenroth <joerg@lauenroth.org>
2021-02-28 23:19:53 +0100

    use svelte

---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script>
+</script>
+
+<header class="main">
+  <h1>Spielstand</h1>
+</header>
+
+<table>
+  <thead>
+    <tr>
+      <th />
+      <th>Anne</th>
+      <th>Juli</th>
+      <th>Nele</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>20</td>
+      <td>25</td>
+      <td>30</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>20</td>
+      <td>15</td>
+      <td>10</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>10</td>
+      <td>15</td>
+      <td>20</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td>∑</td>
+      <td>50</td>
+      <td>55</td>
+      <td>60</td>
+    </tr>
+    <tr class="places">
+      <td />
+      <td>3</td>
+      <td>2</td>
+      <td class="winner">1</td>
+    </tr>
+  </tfoot>
+</table>
+
+<style>
+</style>
