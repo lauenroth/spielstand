d0b5aec20d35666143e11fc2a7859f903ac0ed11
Jörg Lauenroth <joerg.lauenroth@diva-e.com>
2024-01-01 20:07:39 +0100

    button positioning

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
   <MainLayout>
-    <NuxtLink to="new-game" class="btn">New Game</NuxtLink>
+    <p class="new">
+      <NuxtLink to="/new-game" class="btn">New Game</NuxtLink>
+    </p>
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '~/components/MainLayout/MainLayout.vue';
 </script>
+
+<style lang="scss">
+.new {
+  margin: auto 12px 48px;
+}
+</style>
